ed5f99065403b4d53c15f20d97a542b433661b9a
Jasleen Singh <67190606+jasleensingh96@users.noreply.github.com>
2022-03-24 15:21:18 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,6 +2,7 @@ import requests
 from datetime import date, timedelta
 import json
 from tqdm.notebook import tqdm
+from bs4 import BeautifulSoup
 import pandas as pd
 
 def daterange(start_date, end_date):
@@ -17,20 +18,28 @@ end_date = date(2022, 3, 23)
 dct = {}
 dts = [dt for dt in daterange(start_date, end_date)]
 print(len(dts))
+
+
 for single_date in tqdm(dts):
     dt = single_date.strftime("%Y-%m-%d")
     txt = requests.get(url.format(dt)).text
-    txt = txt.split('var hours = ')[-1].split('startHour')[0].strip().rstrip(',')
-    dct[dt] = json.loads(txt)
+    
+    soup = BeautifulSoup(txt, "html.parser")
+    sunrise = soup.find('span', {"class": "sunrise swip"}).find('span', {"class": "time"}).text.strip()
+    sunset = soup.find('span', {"class": "sunset swap"}).find('span', {"class": "time"}).text.strip()
 
-dc = {}
-for k, v in dct.items():
-    for r in v:
-        dc[r['time']] = r
+    txt = txt.split('var hours = ')[-1].split('startHour')[0].strip().rstrip(',')
+    dct_tmnppp = json.loads(txt)
+    for i in dct_tmnppp:
+        i['sunrise'] = sunrise
+        i['sunset'] = sunset
+
+        dct[i['time']] = i
 
 dctt = {}
-for i in list(dc.values()):
-    kys = ['time', 'summary', 'icon', 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature', 'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover', 'uvIndex', 'visibility', 'ozone', 'precipType', 'solar']
+for i in list(dct.values()):
+    kys = ['time', 'summary','sunrise', 'sunset', 'icon', 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature', 'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover', 'uvIndex', 'visibility', 'ozone', 'precipType', 'solar']
     for ky in kys:
         dctt[ky] = dctt.get(ky, []) + [i.get(ky, 'NAN')]
+
 pd.DataFrame(dctt).to_csv('temp.csv', index=False)
